343000d0c18338d36646aa243e998ddcb2ba4f8d
VladanT3 <vladt.t33@gmail.com>
2024-05-26 20:20:17 +0200

    Created server

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/VladanT3/Go_WebServer
 
 go 1.22.3
+
+require github.com/joho/godotenv v1.5.1
+
+require (
+	github.com/go-chi/chi/v5 v5.0.12 // indirect
+	github.com/go-chi/cors v1.2.1 // indirect
+)
--- main.go
+++ main.go
@@ -2,8 +2,43 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	chi "github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	fmt.Println("Hello world")
+	godotenv.Load(".env")
+
+	var port string = os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("PORT not found in environment")
+	}
+
+	router := chi.NewRouter()
+
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"*"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+
+	server := &http.Server{
+		Handler: router,
+		Addr:    ":" + port,
+	}
+
+	fmt.Printf("Server running on: http://localhost:%v\n", port)
+
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
