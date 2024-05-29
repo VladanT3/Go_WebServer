7843d0603b6f9275e48072d8fd1ee7e1d0a6e5fb
VladanT3 <vladt.t33@gmail.com>
2024-05-29 23:04:06 +0200

    Finally set up everything db related

--- main.go
+++ main.go
@@ -1,15 +1,23 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 
+	"github.com/VladanT3/Go_WebServer/internal/database"
 	chi "github.com/go-chi/chi/v5"
 	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
+
+    _ "github.com/lib/pq"
 )
+
+type apiConfig struct {
+    DB *database.Queries
+}
 
 func main() {
 	godotenv.Load(".env")
@@ -18,6 +26,20 @@ func main() {
 	if port == "" {
 		log.Fatal("PORT not found in environment")
 	}
+
+ 	var connStr string = os.Getenv("DB_URL")
+	if connStr == "" {
+		log.Fatal("DB connection string not found in environment")
+	}   
+
+    dbConn, err := sql.Open("postgres", connStr)
+    if err != nil {
+        log.Fatal("Couldn't connect to database")
+    }
+
+    apiCfg := apiConfig{
+        DB: database.New(dbConn),
+    }
 
 	router := chi.NewRouter()
 
@@ -33,6 +55,7 @@ func main() {
 	v1Router := chi.NewRouter()
 	v1Router.Get("/healthz", handlerReadiness)
 	v1Router.Get("/error", handlerError)
+    v1Router.Post("/users", apiCfg.handlerCreateUser)
 
 	router.Mount("/v1", v1Router)
 
@@ -43,7 +66,7 @@ func main() {
 
 	fmt.Printf("Server running on: http://localhost:%v\n", port)
 
-	err := server.ListenAndServe()
+	err = server.ListenAndServe()
 	if err != nil {
 		log.Fatal(err)
 	}
