52d2c73c0e69be348c378cef84c6ed42ae5a640e
VladanT3 <vladt.t33@gmail.com>
2024-06-03 20:40:10 +0200

    Handled duplicate key error

--- scraper.go
+++ scraper.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"database/sql"
 	"log"
+	"strings"
 	"sync"
 	"time"
 
@@ -67,6 +68,9 @@ func scrapeFeed(wg *sync.WaitGroup, db *database.Queries, feed database.Feed) {
             FeedID: feed.ID,
         })
         if err != nil {
+            if strings.Contains(err.Error(), "duplicate key") {
+                continue
+            }
             log.Printf("Error creating new post: %v\n", err)
         }
     }
